616b97e5417fb471f9f8fe03721c15e52e47afd0
Aayush-Ovhal <61664556+Aayush-Ovhal@users.noreply.github.com>
2020-06-01 21:00:21 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,42 @@
+class Form{
+
+    constructor(){
+
+        this.input = createInput("name");
+        this.button = createButton('Submit');
+        this.title2 = createElement('h3');
+
+    }
+
+    hide(){
+        this.title2.hide();
+        this.input.hide();
+        this.button.hide();
+    }
+
+    display(){
+        var title = createElement('h2');
+        title.html("CAR RACING GAME made by noob");
+        title.position(displayWidth/2,0);
+
+        this.input.position(displayWidth/2 - 40,displayHeight/2 - 80);
+
+        this.button.position(displayWidth/2,displayHeight/2);
+        
+        this.button.mousePressed(()=>{
+            this.input.hide();
+            this.button.hide();
+
+            player.name = this.input.value();
+
+            playerCount += 1;
+            player.index = playerCount;
+            player.update();
+            player.updateCount(playerCount);
+
+            this.title2.html("NOMOSKAR " + player.name);
+            this.title2.position(displayWidth/2 - 70,displayHeight/5);
+        })
+    }
+
+}
